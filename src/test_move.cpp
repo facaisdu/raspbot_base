ff83ba8852e80b50fe81fb651cc8898ffbe1bf81
Vince Yang <facai_sdu@126.com>
2018-06-20 16:28:43 +0800

    Create test_move.cpp

---
+++ src/test_move.cpp
@@ -0,0 +1,55 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <softPwm.h>
+
+#define PWMA 1 
+#define AIN1 3 
+#define AIN2 2 
+
+#define PWMB 4 
+#define BIN1 6 
+#define BIN2 5 
+using namespace std;
+
+
+int main()
+
+{
+	wiringPiSetup();
+	pinMode(PWMA,OUTPUT);
+	pinMode(AIN1,OUTPUT);
+	pinMode(AIN2,OUTPUT);
+	pinMode(PWMB,OUTPUT);
+	pinMode(BIN2,OUTPUT);
+	pinMode(BIN1,OUTPUT);
+
+	softPwmCreate(PWMA,0,100);
+	softPwmCreate(PWMB,0,100);
+
+	softPwmWrite(PWMA,50);
+	digitalWrite(AIN1,LOW);
+	digitalWrite(BIN1,HIGH);
+
+	softPwmWrite(PWMB,50);
+	digitalWrite(AIN2,LOW);
+	digitalWrite(BIN2,HIGH);
+	delay(5000);
+
+
+
+
+
+	return 0;
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+About
